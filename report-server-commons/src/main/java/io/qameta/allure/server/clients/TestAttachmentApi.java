7cab97ff98cc8005909748dfaa9b55f6bfdc0b68
Igor Merkushev <igor.merkushev@auto1.com>
2020-07-10 19:20:54 +0200

    change api paths

--- report-server-commons/src/main/java/io/qameta/allure/server/clients/TestAttachmentApi.java
+++ report-server-commons/src/main/java/io/qameta/allure/server/clients/TestAttachmentApi.java
@@ -13,7 +13,7 @@ import static org.springframework.http.HttpStatus.NO_CONTENT;
 public interface TestAttachmentApi { ;
 
     @ResponseStatus(CREATED)
-    @RequestLine("POST /report/attachment/{folderName}/{fileName}")
+    @RequestLine("POST /api/report/attachment/{folderName}/{fileName}")
     @Headers({"Content-Type: multipart/form-data",
         "Accept: application/json"})
     String uploadAttachment(@Param(value = "file") File file,
@@ -21,13 +21,13 @@ public interface TestAttachmentApi { ;
                             @Param("fileName") String fileName);
 
     @ResponseStatus(CREATED)
-    @RequestLine("POST /report/attachment/{folderName}")
+    @RequestLine("POST /api/report/attachment/{folderName}")
     @Headers({"Content-Type: multipart/form-data",
         "Accept: application/json"})
     String uploadAttachment(@Param("file") File file,
                             @Param("folderName") String folderName);
 
     @ResponseStatus(NO_CONTENT)
-    @RequestLine("DELETE /report/attachment/{runId}/{fileName}")
+    @RequestLine("DELETE /api/report/attachment/{runId}/{fileName}")
     void deleteAttachment(@Param("runId") String runId, @Param("fileName") String fileName);
 }
--- report-server-commons/src/main/java/io/qameta/allure/server/clients/TestRunApi.java
+++ report-server-commons/src/main/java/io/qameta/allure/server/clients/TestRunApi.java
@@ -12,26 +12,26 @@ import java.util.List;
 public interface TestRunApi {
 
     @ResponseStatus(HttpStatus.CREATED)
-    @RequestLine("POST /report/test-run")
+    @RequestLine("POST /api/report/test-run")
     @Headers({"Content-Type: application/json", "Accept: application/json"})
     TestRunDTO createTestRun(TestRunDTO testRunDTO);
 
     @ResponseStatus(HttpStatus.OK)
-    @RequestLine("GET /report/test-run/{id}")
+    @RequestLine("GET /api/report/test-run/{id}")
     @Headers({"Content-Type: application/json", "Accept: application/json"})
     TestRunDTO getTestRun(@Param("id") String id);
 
     @ResponseStatus(HttpStatus.OK)
-    @RequestLine("PUT /report/test-run/{id}")
+    @RequestLine("PUT /api/report/test-run/{id}")
     @Headers({"Content-Type: application/json", "Accept: application/json"})
     TestRunDTO updateTestRun(@Param("id") String id, TestRunDTO testRunDTO);
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    @RequestLine("DELETE /report/test-run/{id}")
+    @RequestLine("DELETE /api/report/test-run/{id}")
     void deleteTestRun(@Param("id") String id);
 
     @ResponseStatus(HttpStatus.OK)
-    @RequestLine("GET /report/test-run/all")
+    @RequestLine("GET /api/report/test-run/all")
     @Headers({"Content-Type: application/json", "Accept: application/json"})
     List<TestRunDTO> getAll();
 }
